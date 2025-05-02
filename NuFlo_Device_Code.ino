1bb00b62400931daccac86f7182b4746676d98da
Music0de <ian@arkansasmakersprep.org>
2025-05-02 12:16:31 -0500

    NuFlo re.1
    LoRa sending code + pH sensor code

---
+++ NuFlo_Device_Code.ino
@@ -0,0 +1,109 @@
+//------ LORA Stuff
+#include "LoRaWan_APP.h"
+#include "Arduino.h"
+
+#define RF_FREQUENCY               915000000  // Hz
+#define TX_OUTPUT_POWER            21         // dBm
+#define LORA_BANDWIDTH             0          // [0: 125 kHz]
+#define LORA_SPREADING_FACTOR      7          // [SF7..SF12]
+#define LORA_CODINGRATE            1          // [1: 4/5]
+#define LORA_PREAMBLE_LENGTH       8
+#define LORA_SYMBOL_TIMEOUT        0
+#define LORA_FIX_LENGTH_PAYLOAD_ON false
+#define LORA_IQ_INVERSION_ON       false
+#define RX_TIMEOUT_VALUE           1000
+#define BUFFER_SIZE                85
+
+//------- Pin Definitions (use actual ESP32 ADC1 pins)
+#define turbidityPin    32   // ADC1_CH4
+#define temperaturePin  33   // ADC1_CH5
+#define phPin           34   // ADC1_CH6
+
+#define DELAY_TIME      2000  // ms
+
+// Sensor readings
+int   rawTurbidity;
+int   rawTemp;
+int   rawPH;
+float voltageTemp;
+float temperatureC;
+float phValue;
+
+// LoRa variables
+char  txPacket[BUFFER_SIZE];
+bool  loraIdle = true;
+unsigned long txCount = 0;
+
+static RadioEvents_t RadioEvents;
+void OnTxDone( void );
+void OnTxTimeout( void );
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(turbidityPin,   INPUT);
+  pinMode(temperaturePin, INPUT);
+  pinMode(phPin,          INPUT);
+
+  // LoRa init
+  Mcu.begin(HELTEC_BOARD, SLOW_CLK_TPYE);
+  RadioEvents.TxDone    = OnTxDone;
+  RadioEvents.TxTimeout = OnTxTimeout;
+  Radio.Init(&RadioEvents);
+  Radio.SetChannel(RF_FREQUENCY);
+  Radio.SetTxConfig(
+    MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+    LORA_SPREADING_FACTOR, LORA_CODINGRATE,
+    LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
+    true, 0, 0, LORA_IQ_INVERSION_ON, 3000
+  );
+}
+
+void loop() {
+  // --- 1) Read raw ADC ---
+  rawTurbidity = analogRead(turbidityPin);
+  rawTemp      = analogRead(temperaturePin);
+  rawPH        = analogRead(phPin);
+
+  // --- 2) Temperature conversion (unchanged) ---
+  voltageTemp   = rawTemp * (3.3 / 4095.0);
+  temperatureC  = (voltageTemp - 0.5) * 100.0;
+
+  // --- 3) pH conversion using your UNO‐calibrated formula ---
+  //    a) convert 12-bit down to 10-bit
+  int sensorValue10 = rawPH >> 2;  
+  //    b) voltage on a 5V/1023 scale
+  float voltagePH   = sensorValue10 * (5.0 / 1023.0);
+  //    c) your exact linear fit
+  phValue           = (-5.76923 * voltagePH) + 21.48077;
+
+  // --- 4) Send via LoRa when ready ---
+  if (loraIdle) {
+    delay(1000);
+    int len = snprintf(txPacket, BUFFER_SIZE,
+      "NUFLO1 | Turb:%d | T:%.1fC | pH:%.2f | #%lu",
+      rawTurbidity,
+      temperatureC,
+      phValue,
+      txCount
+    );
+    Serial.printf("\nSending packet \"%s\" (len=%d)\n", txPacket, len);
+    Radio.Send((uint8_t*)txPacket, len);
+    txCount++;
+    loraIdle = false;
+  }
+
+  Radio.IrqProcess();
+  delay(DELAY_TIME);
+}
+
+void OnTxDone(void) {
+  Serial.println("TX done");
+  loraIdle = true;
+}
+
+void OnTxTimeout(void) {
+  Radio.Sleep();
+  Serial.println("TX Timeout");
+  loraIdle = true;
+}
